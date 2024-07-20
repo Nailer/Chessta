7410d92441ce52a3b8c41ac1dbc46da0d78fd37e
Abisuwa <ayodejiabisuwa23@gmail.com>
2024-07-20 08:10:17 +0100

    chnaged main component to Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,36 @@
-import React from 'react'
-import './App.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-// import Home from './Home'
-import Main from './Main'
-import UserForm from './Userform'
-import GameApp from './GameApp'
-import { useAuthState } from 'react-firebase-hooks/auth'
-import { auth } from './firebase'
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import Main from "./Main";
+import UserForm from "./Userform";
+import GameApp from "./GameApp";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "./firebase";
 
 export default function App() {
-    const [user, loading, error] = useAuthState(auth)
+  const [user, loading, error] = useAuthState(auth);
 
-    if (loading) {
-        return 'loading ...'
-    }
-    if (error) {
-        return 'There was an error'
-    }
-    if (!user) {
-        return <UserForm />
-    }
+  if (loading) {
+    return "loading ...";
+  }
+  if (error) {
+    return "There was an error";
+  }
+  if (!user) {
+    return <UserForm />;
+  }
 
-    return (
-        <Router>
-            <Switch>
-                <Route exact path="/">
-                    <Main></Main>
-                </Route>
-                <Route path="/game/:id">
-                    <GameApp />
-                </Route>
-            </Switch>
-        </Router>
-    )
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/game/:id">
+          <GameApp />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
