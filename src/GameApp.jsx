17affe5ada901bd726394e98e99e705ab3c50932
Abisuwa <ayodejiabisuwa23@gmail.com>
2024-07-21 21:49:18 +0100

    added  useRef import from react

--- src/GameApp.jsx
+++ src/GameApp.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
 import { gameSubject, initGame, resetGame } from "./Game";
 import Board from "./Board";
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState } from "react";
 import { auth, db } from "./firebase";
 import { useHistory } from "react-router-dom";
 
