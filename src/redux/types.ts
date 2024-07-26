b010c0762007c9b767f37165f87d218742f967f7
Abisuwa <ayodejiabisuwa23@gmail.com>
2024-07-26 12:56:01 +0100

    (fix) - removed unused slices

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,31 +1,5 @@
-export type RecipientT = {
-  name: string;
-  publicKey: any;
-  amountDonated: number;
-  amountRequired: number;
-  description: string;
-  donationComplete: boolean;
-};
-
-export type CampaignT = {
-  pubKey: string;
-  name: string;
-  amountDonated: number;
-  amountRequired: number;
-  description: string;
-  donationComplete: boolean;
-  id: number;
-};
-
 export type EdgeCloudT = {
   ingestorId: string;
   streamServer: string;
   streamKey: string;
-};
-
-export type TransactionT = {
-  transactionNo: number;
-  time: Date;
-  status: string;
-  signature: string;
 };
